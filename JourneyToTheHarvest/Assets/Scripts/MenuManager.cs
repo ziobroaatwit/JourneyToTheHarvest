530865e4009347a3038db45b6674270301e8a071
ziobroaatwit <ziobroa@wit.edu>
2023-04-05 17:56:57 -0400

    Menu BG and button hookup

--- JourneyToTheHarvest/Assets/Scripts/MenuManager.cs
+++ JourneyToTheHarvest/Assets/Scripts/MenuManager.cs
@@ -6,7 +6,7 @@ public class MenuManager : MonoBehaviour
 {
     public void onPlayClick()
     {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("mainField");
     }
     public void onQuitClick()
     {
